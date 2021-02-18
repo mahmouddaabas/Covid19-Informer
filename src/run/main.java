5837859f49415328a4f7f0438acac2d9ad64974a
Mahmoud Daabas <77792515+mahe0406@users.noreply.github.com>
2021-02-18 03:35:07 +0100

    Update 1.0
    - Added initial code.
    
    - Added a GUI with seach functions for Country and Country Code.
    
    - Added JLabels with Initial Data. (Will include global data).
    
    - Added a inner class in GUI that allows you to change background of the program, has 3 ways, a png from the computer, a png from the classpath or a URL.
    
    - Added methods to create listeners.
    
    - Added method to set default data.
    
    - Added .gitignore file to the source.

---
+++ src/run/main.java
@@ -0,0 +1,13 @@
+package run;
+
+import gui.*;
+
+import java.io.IOException;
+
+public class main
+{
+    public static void main(String[] args) throws IOException {
+
+        GUI run = new GUI();
+    }
+}
